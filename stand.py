cd83e65870722fd7fd8fe72e973e518ce4c234fc
Alena Shevelko <mursilka481516@gmail.com>
2025-04-10 21:24:07 +0100

    fix test and requests

--- stand.py
+++ stand.py
@@ -2,14 +2,13 @@
 
 import requests
 import configuration
-from data import order_data
 
 # Создаем заказ, отправляя POST-запрос на указанный URL-адрес.
-def create_order():
-    response = requests.post(f"{configuration.URL_SERVICE}{configuration.ORDERS_URL}", json=order_data)
-    return response.status_code, response.json()
+def create_order(order_data):
+    response = requests.post(f"{configuration.URL_SERVICE}/{configuration.ORDERS_URL}", json=order_data)
+    return response.json()
 
 # Получаем информацию о заказе по его трек-номеру, отправляя GET-запрос на указанный URL-адрес.
 def get_order_by_track(track):
-    response = requests.get(f"{configuration.URL_SERVICE}{configuration.TRACK_URL}{track}")
-    return response.status_code, response.json()
+    response = requests.get(f"{configuration.URL_SERVICE}/{configuration.TRACK_URL}{track}")
+    return response.status_code
--- test_order.py
+++ test_order.py
@@ -1,16 +1,18 @@
 # Алёна Шевелько, 28-когорта - Финальный проект. Инженер по тестированию плюс
 
 from stand import create_order, get_order_by_track
+from data import order_data
 
 def test_get_order_by_track():
     # Создаем заказ и получаем трекинговый номер
-    track = create_order()
+    order = create_order(order_data)
+    track = order["track"]
 
     # Проверяем, что трекинговый номер не является пустым
     assert track is not None
 
     # Получаем статус код и JSON-ответ при запросе информации о заказе по трекинговому номеру
-    status_code, _ = get_order_by_track(track)
+    status_code = get_order_by_track(track)
 
     # Проверяем, что статус код равен 200 (успешный запрос)
     assert status_code == 200
