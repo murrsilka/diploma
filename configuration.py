e999c04a6de4e3b21e2900a9804e63c3024466cb
Alena Shevelko <mursilka481516@gmail.com>
2025-04-10 21:22:29 +0100

    configuration: removed slashes

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # Алёна Шевелько, 28-когорта - Финальный проект. Инженер по тестированию плюс
 
-URL_SERVICE = "https://09e8ad26-5f6b-4ea4-8033-ad67c2617ff0.serverhub.praktikum-services.ru/"
-ORDERS_URL = "/api/v1/orders"
-TRACK_URL = "/api/v1/orders/track?t="
+URL_SERVICE = "https://42e5839d-cf5f-415a-980c-a3615d651c8d.serverhub.praktikum-services.ru"
+ORDERS_URL = "api/v1/orders"
+TRACK_URL = "api/v1/orders/track?t="
